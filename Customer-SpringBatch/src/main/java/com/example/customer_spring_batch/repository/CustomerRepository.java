57e150f1faa06c49d0779eda87b2c8758b720ea1
Yashita <jainyashita06@gmail.com>
2024-07-12 11:30:11 +0530

    chore: java-8 features

--- Customer-SpringBatch/src/main/java/com/example/customer_spring_batch/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.customer_spring_batch.repository;
-
-
-import com.example.customer_spring_batch.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
-}
